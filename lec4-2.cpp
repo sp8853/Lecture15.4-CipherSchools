56b73f295071f191c6fd4f36dc101c4f4e3a0081
sp8853 <103991911+sp8853@users.noreply.github.com>
2023-08-10 23:19:36 +0530

    Add files via upload

---
+++ lec4-2.cpp
@@ -0,0 +1,19 @@
+CONDITIONAL STATEMENT 
+execute stuff
+
+what is boolean algebra and why is it important 
+AND-&&
+OR-||
+
+0+0-0
+0+1=1
+1+0=0
+1+1=1
+
+0*0=0
+1*0=0
+0*1=0
+1*1=1
+
+GOT to know how if -else statement works
+if(conditional statement )
